5422334507dd9668a750b0609c03c4e3c5d4482f
manza <artemisholmes32@gmail.com>
2022-02-16 21:16:40 -0600

    4th commit

--- src/git/testing.java
+++ src/git/testing.java
@@ -5,7 +5,7 @@ public class testing {
 	public static void main(String[] args) {
 		
 		System.out.println("Water is colourless");
-		System.out.println("Water is blue");
+		System.out.println("Water is orange");
 		
 //      feature
 		System.out.println("Water is red");
